8145cc3769138a51a0af35e48660b8f4d3fa9e2b
Vladislav Dedikov <69351396+crew-dev@users.noreply.github.com>
2024-08-30 19:58:50 +0300

    Add files via upload

--- js/counter-timer.js
+++ js/counter-timer.js
@@ -1,15 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
 	spaceBetween: 30,
 	centeredSlides: true,
-	loop: true, // Добавили loop
+	loop: true,
 	navigation: {
-		nextEl: '.custom-next', // Используем кастомную стрелку
+		nextEl: '.custom-next',
 		prevEl: ".custom-prev"
 	},
-	on: {
-		autoplayTimeLeft(s, time, progress) {
-			progressCircle.style.setProperty("--progress", 1 - progress)
-			progressContent.textContent = `${Math.ceil(time / 1000)}`
-		}
-	}
 })
